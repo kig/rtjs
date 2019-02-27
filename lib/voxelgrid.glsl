5efcab520686cbd93424329f1d51adf799ea574b
Ilmari Heikkinen <ilmari@fhtr.net>
2019-02-27 18:09:45 +0800

    Minor optimization to intersectGridLeaf

--- lib/voxelgrid.glsl
+++ lib/voxelgrid.glsl
@@ -153,55 +153,43 @@ vec3 triNormal(in Array array, in vec3 point, in int triIndex) {
     return normalize(mix(mix(n0, n2, v), n1, u));
 }
 
-#ifdef DEPTH_FIRST
+#ifndef BREADTH_FIRST
 
-void intersectGridLeaf(in Array array, inout Ray ray, in int headOff, inout Hit closestHit) {
+void intersectGridLeaf(in Array array, inout Ray ray, in int headOff, inout Hit closestHit, ivec3 cstep, vec3 deltaDist) {
     vec3 origin = readVec3(array, headOff);
-    int osize = readInt(array, headOff + 3);
-    int size = abs(osize);
-    bool isLeaf = osize > 0;
+    int size = readInt(array, headOff + 3);
     vec3 dims = readVec3(array, headOff + 4);
     int childSize = readInt(array, headOff + 7);
+    
     float scale = dims.x / float(size);
+
     int voxelsOff = headOff + 8;
     int childIndexOff = voxelsOff + size * size * size;
     int childOff = childIndexOff + childSize;
 
     float t = intersectBox(ray, origin, dims);
-    if (t < 0.0) {
+
+    vec3 cf = toGrid(ray.o + ray.d * (t + 0.001 * scale), scale, origin);
+    ivec3 c = ivec3(cf);
+
+    if (c.x < 0 || c.y < 0 || c.z < 0 || c.x >= size || c.y >= size || c.z >= size) {
         return;
     }
 
-    // Map hit to voxel coordinates
-    Ray tray = ray;
-    tray.o = tray.o + tray.d * (t+0.001*scale);
-
-    vec3 cf = toGrid(tray.o, scale, origin);
-    ivec3 c = ivec3(cf);
-
-    vec3 deltaDist = vec3(
-        length(ray.d / ray.d.x),
-        length(ray.d / ray.d.y),
-        length(ray.d / ray.d.z)
-    );
-    ivec3 cstep = ivec3(sign(ray.d));
     vec3 next = (max(vec3(0.0), vec3(cstep)) + vec3(cstep) * (vec3(c) - cf)) * deltaDist;
 
     int ci = c.z * size * size + c.y * size + c.x;
 
     // Step through the grid while we're inside it
     for (int i = 0; i < 3*size; i++) {
-        if (c.x < 0 || c.y < 0 || c.z < 0 || c.x >= size || c.y >= size || c.z >= size) {
-            return;
+        if (costVis) {
+            ray.light.b += 0.01;
         }
-if (costVis) {
-        ray.light.b += 0.01;
-}
         int vi = readInt(array, voxelsOff + ci);
         if (vi > 0) {
-if (costVis) {
-            ray.light.g += 0.1;
-}
+            if (costVis) {
+                ray.light.g += 0.1;
+            }
             int coff = childIndexOff + (vi - 1) * childSize;
             intersectTris(array, ray, coff, childSize, closestHit);
             if (closestHit.index >= 0) {
@@ -216,19 +204,31 @@ if (costVis) {
                 next.x += deltaDist.x;
                 c.x += cstep.x;
                 ci += cstep.x;
+                if (c.x < 0 || c.x >= size) {
+                    return;
+                }
             } else {
                 next.z += deltaDist.z;
                 c.z += cstep.z;
                 ci += cstep.z * size * size;
+                if (c.z < 0 || c.z >= size) {
+                    return;
+                }
             }
         } else if (next.y < next.z) {
             next.y += deltaDist.y;
             c.y += cstep.y;
             ci += cstep.y * size;
+            if (c.y < 0 || c.y >= size) {
+                return;
+            }
         } else {
             next.z += deltaDist.z;
             c.z += cstep.z;
             ci += cstep.z * size * size;
+            if (c.z < 0 || c.z >= size) {
+                return;
+            }
         }
     }
 }
@@ -250,11 +250,7 @@ void intersectGridNode(in Array array, inout Ray ray, in int headOff, inout Hit 
         return;
     }
 
-    // Map hit to voxel coordinates
-    Ray tray = ray;
-    tray.o = tray.o + tray.d * (t+0.001*scale);
-
-    vec3 cf = toGrid(tray.o, scale, origin);
+    vec3 cf = toGrid(ray.o + ray.d * (t + 0.001 * scale), scale, origin);
     ivec3 c = ivec3(cf);
 
     vec3 deltaDist = vec3(
@@ -272,32 +268,18 @@ void intersectGridNode(in Array array, inout Ray ray, in int headOff, inout Hit 
         if (c.x < 0 || c.y < 0 || c.z < 0 || c.x >= size || c.y >= size || c.z >= size) {
             return;
         }
-if (costVis) {
-        ray.light.b += 0.01;
-}
+        if (costVis) {
+            ray.light.b += 0.01;
+        }
         int vi = readInt(array, voxelsOff + ci);
         if (vi > 0) {
-            if (isLeaf) {
-if (costVis) {
-                ray.light.g += 0.1;
-}
-                int coff = childIndexOff + (vi - 1) * childSize;
-                intersectTris(array, ray, coff, childSize, closestHit);
-                if (closestHit.index >= 0) {
-                    ivec3 p = ivec3(toGrid(ray.o + ray.d * closestHit.distance, scale, origin));
-                    if (all(equal(p, c))) {
-                        return;
-                    }
-                }
-            } else {
-if (costVis) {
+            if (costVis) {
                 ray.light.b += 0.1;
-}
-                int coff = childOff + readInt(array, childIndexOff + vi - 1);
-                intersectGridLeaf(array, ray, coff, closestHit);
-                if (closestHit.index >= 0) {
-                    return;
-                }
+            }
+            int coff = childOff + readInt(array, childIndexOff + vi - 1);
+            intersectGridLeaf(array, ray, coff, closestHit, cstep, deltaDist);
+            if (closestHit.index >= 0) {
+                return;
             }
         }
         if (next.x < next.y) {
